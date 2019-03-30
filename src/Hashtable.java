e98de1ab18d0eca0f4c9bc3255d961581310e030
Takudzwa Barnabas Shumbamhini <shmtak004@sl-dual-206.cs.uct.ac.za>
2019-03-30 15:44:55 +0200

    Sat changes

--- src/Hashtable.java
+++ src/Hashtable.java
@@ -11,6 +11,26 @@ import java.util.*;
 
 public class Hashtable{
 
+	class Data{
+
+		String date;
+		String power;
+		String voltage;
+		
+		public Data(){
+			date= null;
+			power=null;
+			voltage=null;
+		}
+		
+		public Data(String d, String p, String v){
+			date = d;
+			power=p;
+			voltage=v;
+		}
+
+	}
+
 	public static Data t;
 	static int[] primes=new int[52];
 	/**
@@ -75,24 +95,58 @@ try{
 	*/
 	public static void makeTable(int table_size){
 		String line = null;
-		ArrayList <Data> table = new ArrayList<Data>(table_size);
+
+		Data[] dates = new Data[table_size];
+		//adjust for type data not string
+		int no_of_probes=0;
 		try{
 			BufferedReader filereader = new BufferedReader(new FileReader("tabledata/data.txt"));
 			filereader.readLine();
 			while ((line = filereader.readLine())!=null){
+			no_of_probes=0;
 			String [] lineinfo = line.split(",");
 			t=new Data(lineinfo[0], lineinfo[1], lineinfo[2]);
+			if ( dates[hash_key(lineinfo[0],table_size)]==null ){dates[hash_key(lineinfo[0],table_size)]=t; System.out.println(hash_key(lineinfo[0],table_size));}
+			else{
+				System.out.println(hash_key(lineinfo[0],table_size)+" is taken already");
+				int hash_No = hash_key(lineinfo[0],table_size);
+				int i = 1;
+				while(dates[hash_No]!=null){
+					hash_No = (hash_No+i)%table_size;
+					if(dates[hash_No]==null){dates[hash_No]=t;System.out.println("position now found after probes");break;}
+					else{i++;no_of_probes++;}
+				}
+				
+			}
+			System.out.println("Number of linear probes made is "+no_of_probes);
 			System.out.println(t.date+" "+t.power+" "+t.voltage);
 			}
 		}
 		catch (Exception e){ e.printStackTrace();}
 	}
-	public static void main(String[] args){
-		extractData();
-		isPrime();
-		checkPrime(997);
-		makeTable(997);
+
+	/**
+	method to hash.
+	@param key the string key to be hashed
+	@return hashVal % tableSize returns position of array
+	*/
+	public static int hash_key ( String date, int tableSize ){
+
+		int hashVal = 0;
+		for( int i = 0; i < date.length(); i++ )
+			hashVal += date.charAt(i);
+		return hashVal % tableSize;
 	}
 
+	/**
+	method for linear probing, the big one
+	*/
+	//public int linear_probe(int hash){}
 
+	public static void main(String[] args){
+		isPrime();
+		extractData();
+		makeTable(653);
+
+	}
 }
