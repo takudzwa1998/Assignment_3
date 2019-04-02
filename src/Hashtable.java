e302622cd2b030b3c7ed82ed1bfe9508cae539b2
Takudzwa Barnabas Shumbamhini <shmtak004@sl-dual-208.cs.uct.ac.za>
2019-04-03 00:49:26 +0200

    Wed Changes 3

--- src/Hashtable.java
+++ src/Hashtable.java
@@ -65,43 +65,60 @@ try{
 	*/
 	public static boolean checkPrime(int n){
 		for (int p:primes){
-			if(p==n){System.out.println(n+" IS a prime number between 653 and 1009");return true;}
+			if(p==n){return true;}
 		}	
-		System.out.println(n+" IS NOT a prime number between 653 and 1009");
+		System.out.println(n+" IS NOT a prime number between 653 and 1009, please select an appropriate number.");
 		return false;
 	}
 
+	public static void Random_KeyGen(int tableSize,String filename){
+		String[] keys=new String[500];
+		String line = null;
+		try{
+
+			BufferedReader filereader = new BufferedReader(new FileReader("tabledata/"+filename));
+			int num=0;
+			while ((line = filereader.readLine())!=null){
+				String [] lineinfo = line.split(",");
+				keys[num]=lineinfo[0];
+				num++;
+			}
+			List<String> strList = Arrays.asList(keys);
+			Collections.shuffle( strList, new Random(tableSize) );
+			for(String x: strList){System.out.println(x);}
+		}
+		
+		catch (Exception e){ e.printStackTrace();}
+		
+	}
 	
 	/**
 	method to make hash Table of given size
 	@param table_size takes in the table size from the command line arguments	
 	@param probe_Type sets the type of probing used for collsion resolution
 	*/
-	public static void makeTable(int table_size, String probe_Type, String filename){
+	public static void makeTable(int table_size, String probe_Type, String filename, String Search_Status){
 		String line = null;
-
-		Data[] dates = new Data[table_size];
+		 Data[] dates = new Data[table_size];
 		//adjust for type data not string
 		int no_of_quad_probes=0;
 		int no_of_linear_probes=0;
 		double positions_Taken=0;
 		try{
-			//FileReader file = new FileReader(filename);
 			BufferedReader filereader = new BufferedReader(new FileReader("tabledata/"+filename));
 			while ((line = filereader.readLine())!=null){
-			//no_of_probes=0;
 			String [] lineinfo = line.split(",");
 			t=new Data(lineinfo[0], lineinfo[1], lineinfo[2]);
-			if ( dates[hash_key(lineinfo[0],table_size)]==null ){dates[hash_key(lineinfo[0],table_size)]=t; 
+			if ( dates[hash_key(lineinfo[0],table_size)]==null ){
+				dates[hash_key(lineinfo[0],table_size)]=t; 
 				positions_Taken=positions_Taken+1.00;}
 			else{
-
+				//LINEAR PROBE IMPLEMENTATION
 				int hash_No = hash_key(lineinfo[0],table_size);
 				int i = 1;
-				//Implementing linear probe here
 				if(probe_Type.equals("linear_Probe")){
 					while(dates[hash_No]!=null){
-						hash_No = (hash_No+i)%table_size;//hash func for linear probe
+						hash_No = hash_key_Lprobe (table_size, hash_No, i );//hash func for linear probe
 						if(dates[hash_No]==null){dates[hash_No]=t;
 							positions_Taken=positions_Taken+1.00;break;}
 						else{i++;no_of_linear_probes++;}
@@ -111,10 +128,10 @@ try{
 
 				else{
 
-					//here we are implementing the apparently more efficient quadratic probing method
+					//QUADRATIC PROBE IMPLEMENTATION
 					if(probe_Type.equals("quad_Probe")){
 						while(dates[hash_No]!=null){
-							hash_No = ( hash_No+(i<<1)-1 )%table_size;//hash func for quad probe
+							hash_No = hash_key_Qprobe ( table_size, hash_No, i );//hash func for quad probe
 							if(dates[hash_No]==null){dates[hash_No]=t;
 								positions_Taken=positions_Taken+1.00;break;}
 							else{i++;no_of_quad_probes++;
@@ -125,11 +142,36 @@ try{
 			}
 			if (no_of_quad_probes>table_size){System.out.println("Probes exceeding table size,please choose bigger table size");}
 			else{
-				if(probe_Type.equals("quad_Probe")){System.out.println("Number of total quadratic probes made is "+no_of_quad_probes);}
-				if(probe_Type.equals("linear_Probe")){System.out.println("Number of total linear probes made is "+no_of_linear_probes);}
-			double load_factor = positions_Taken/(double)table_size;
-			DecimalFormat lf = new DecimalFormat("###.###");
-			System.out.println("Load factor for the hash table of size "+table_size+" is "+lf.format(load_factor) );
+				double load_factor = positions_Taken/(double)table_size;
+				DecimalFormat lf = new DecimalFormat("###.###");
+				if(probe_Type.equals("quad_Probe")){System.out.print(lf.format(load_factor)+";"+no_of_quad_probes);}
+				if(probe_Type.equals("linear_Probe")){System.out.print(lf.format(load_factor)+";"+no_of_linear_probes);}
+			}
+			if( Search_Status.equals("null") ){}
+			else{
+			//SEARCH CODE
+
+			String line_two= null;
+			try{
+				BufferedReader filereader_two = new BufferedReader( new FileReader("GraphInfo/Rand_"+Integer.toString(table_size)+"_Key.txt") );
+				int numKeys=0;
+				int probe=0;
+					while ((line_two = filereader_two.readLine())!=null){
+						numKeys++;
+						if(numKeys==401){break;}
+						else{
+							for(int t=0;t<table_size;t++){
+							if(dates[t]==null){probe++;}else{
+							if ( line_two.equals(dates[t].date) ){probe++;break;}
+								else{probe++;}
+							}}
+						}
+				}
+					System.out.println(";"+probe);
+			
+			}
+			catch (Exception e){ e.printStackTrace();}
+
 			}
 		}
 		catch (Exception e){ e.printStackTrace();}
@@ -144,20 +186,26 @@ try{
 	
 		int hashVal = 0;
 		int powerNum=31;
-		for( int i = 0; i < date.length(); i++ )
-			hashVal += ( powerNum*hashVal )+date.charAt(i);
-		String newString = date.substring ( 11, date.length() );
-		String str = newString.replaceAll("\\D+","");
-		String str2 = str.replaceAll("0","");
-
-		//for( int i = 0; i < str2.length(); i++ )
-		//	hashVal += ( 4*str2.charAt(i) ) + ( str2.charAt(i)/2 ) +tableSize+ (hashVal % tableSize/2) ;
-		if(hashVal<0){hashVal= -1*hashVal;}
-		return (hashVal) % tableSize;
+		for( int i = 0; i < date.length(); i++ ){
+			hashVal += ( powerNum*hashVal )+date.charAt(i) ;
+		}
+		if(hashVal<0){hashVal=-1*hashVal;}
+		return hashVal%tableSize;
 	}
 
-	public static void chaining_Table(int table_size){
-		System.out.println("Enter the chaining");		
+	public static int hash_key_Lprobe ( int tableSize, int RemValue, int increment ){
+		return (RemValue+increment) % tableSize;
+	}
+
+	public static int hash_key_Qprobe ( int tableSize, int RemValue, int increment ){
+		return ( RemValue+(increment*increment) ) % tableSize;
+	}
+
+
+
+
+
+	public static void chaining_Table(int table_size,String filename, String Search_Status){		
 		String line = null;
 		LinkedList[] dates = new LinkedList[table_size];
 
@@ -165,7 +213,7 @@ try{
 		int no_of_chain_probes=0;
 		double positions_Taken=0;
 		try{
-			BufferedReader filereader = new BufferedReader(new FileReader("tabledata/data.txt"));
+			BufferedReader filereader = new BufferedReader(new FileReader("tabledata/"+filename));
 			for (int lists =0;lists<table_size;lists++){				
 				LinkedList <Data> key_list = new LinkedList<>();
 				dates[lists]=key_list;
@@ -175,21 +223,47 @@ try{
 				String [] lineinfo = line.split(",");
 				t=new Data(lineinfo[0], lineinfo[1], lineinfo[2]);		
 				dates[hash_key(lineinfo[0],table_size)].add(t);
-				positions_Taken=positions_Taken+1.00;
-				no_of_chain_probes++;	
+				positions_Taken=positions_Taken+1.00;	
 			}
-
-			//for (int lists_two =0;lists_two<table_size;lists_two++){
-			//	LinkedList<Data> dSimp = dates[lists_two];
-			//	for(Data obj : dSimp ){
-					//System.out.println( "data at position "+lists_two+" is "+obj.date+" "+obj.power+" "+obj.voltage);}
-			//}
-			System.out.println("Number of chaining probes made is "+no_of_chain_probes);
-			System.out.println(t.date+" "+t.power+" "+t.voltage);
-
+			int num_chains=0;
+			for (int lists_two =0;lists_two<table_size;lists_two++){
+				LinkedList<Data> dSimp = dates[lists_two];
+				if (dSimp.size()>1){
+				no_of_chain_probes= no_of_chain_probes +dSimp.size();				
+				num_chains++;
+				}
+			}
+			if( Search_Status.equals("null") ){}
+			else{
+			//SEARCH CODE
+			String line_two= null;
+			try{
+				BufferedReader filereader_two = new BufferedReader( new FileReader("GraphInfo/Rand_"+Integer.toString(table_size)+"_Key.txt") );
+				int numKeys=0;
+				int probe=0;
+					while ((line_two = filereader_two.readLine())!=null){
+						numKeys++;
+						if(numKeys==401){break;}
+						else{
+							for (int lists_two =0;lists_two<table_size;lists_two++){
+								LinkedList<Data> dSimp = dates[lists_two];
+								for(Data obj : dSimp ){if(obj.date=="null"){probe++;}
+									else{
+										if ( line_two.equals(obj.date) ){probe++;break;}
+											else{probe++;}
+									}
+								}
+							}
+						}
+			
+			}
+			System.out.print(probe+";");
+			}
+			catch (Exception e){ e.printStackTrace();}
+			}
 			double load_factor = positions_Taken/(double)table_size;
 			DecimalFormat lf = new DecimalFormat("###.###");
-			System.out.println("Load factor for table of size "+table_size+" is "+lf.format(load_factor) );
+			System.out.println(lf.format(load_factor)+";"+no_of_chain_probes/10);
 		}
 		catch (Exception e){ e.printStackTrace();}
 	}
@@ -201,8 +275,9 @@ try{
 		isPrime();
 		extractData();
 		if ( checkPrime(Integer.parseInt(args[0])) ){
-			if( args[1].equals("chaining") ){chaining_Table( Integer.parseInt(args[0]) );}
-			else{ makeTable( Integer.parseInt(args[0]), args[1], args[2] ); }
+			if( args[1].equals("chaining") ){chaining_Table( Integer.parseInt(args[0]), args[2], args[3] );}
+			else{ makeTable( Integer.parseInt(args[0]), args[1], args[2], args[3] ); }
+			if( args[1].equals("Rand") ){Random_KeyGen( Integer.parseInt(args[0]), args[2] );}
 		}
 	}
 }
